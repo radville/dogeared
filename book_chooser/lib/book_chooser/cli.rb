23505e6c6978cffe4362d40197a9bbdcaaaa29a3
radville <laura.radville@gmail.com>
2019-08-06 14:00:17 -0400

    added genre menu

--- book_chooser/lib/book_chooser/cli.rb
+++ book_chooser/lib/book_chooser/cli.rb
@@ -6,24 +6,34 @@ class BookChooser::CLI
         main_menu
     end
 
-    def list_books
-        puts "list some books"
+    def list_genres
+        puts "Great! Choose the number of the genre you would like to browse, or type 'exit'"
+        puts "1. Non fiction"
+        input = gets.strip
+        if input == "exit"
+            goodbye
+        elsif input == "1"
+            puts "list books from 1st genre"
+        end
     end
 
     def main_menu
         puts "Would you like help choosing a book? Y/n"
-        input = gets.strip.downcase
-        if input == "y" || input == "yes" || input == ""
-            list_books
-        elsif input == "n" || input == "no"
-            puts "No problem!" + goodbye
-        else
-            puts "Invalid command. Please enter yes or no."
-        end   
+        input = nil
+        until input == "n" || input == "y" || input == ""
+            input = gets.strip.downcase
+            if input == "y" || input == ""
+                list_genres
+            elsif input == "n" || input == "no"
+                goodbye
+            else
+                puts "Invalid command. Please enter yes or no."
+            end  
+        end    
     end
 
     def goodbye
-        "Happy reading!"
+        puts "Happy reading!"
     end
 
 end
