482ecfdb736539f0e19a1ad57b5eb1428cba5c64
radville <laura.radville@gmail.com>
2019-08-07 09:09:09 -0400

    added methods to Book class

--- book_chooser/lib/book_chooser/book.rb
+++ book_chooser/lib/book_chooser/book.rb
@@ -3,9 +3,16 @@ class BookChooser::Book
 
     @@all = []
 
-    def initialize
+    def initialize(book_hash)
+        book_hash.each { |key, value| self.send(("#{key}="), value)}
         BookChooser::Genre.books << self
         @@all << self
+    end
+
+    def self.create_from_book_list(books_array)
+        books_array.each do |book|
+            self.new(book)
+        end
     end
 
     def self.all
